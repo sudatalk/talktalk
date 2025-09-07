a604a1dab5a5a99a340317b74c2247bd92930966
joy.hwan <joy.hwan@kakaocorp.com>
2025-09-07 23:21:54 +0900

    feat : 토론방 참여 모달 RHF 부착 및 프로필 이미지 폼 적용

--- pages/talk-list/components/JoinTalkModal/JoinTalkModalContent.tsx
+++ pages/talk-list/components/JoinTalkModal/JoinTalkModalContent.tsx
@@ -4,23 +4,31 @@ import JoinTalkModalButton from "./JoinTalkModalButton";
 import JoinTalkModalProfileImage from "./JoinTalkModalProfileImage";
 import JoinTalkModalNickname from "./JoinTalkModalNickname";
 import JoinTalkModalTeam from "./JoinTalkModalTeam";
+import { FormProvider, useForm } from "react-hook-form";
+import { JOIN_TALK_FORM_DEFAULT_VALUES } from "../../constants/joinTalkForm";
 
 const JoinTalkModalContent = () => {
+  const form = useForm({
+    defaultValues: JOIN_TALK_FORM_DEFAULT_VALUES,
+  });
+
   const leftTeam = "왼쪽 팀명";
   const rightTeam = "오른쪽 팀명";
 
   return (
-    <View style={styles.container}>
-      <JoinTalkModalHeader />
-      <View style={styles.bodyContainer}>
-        <JoinTalkModalProfileImage selectedIndex={0} />
-        <JoinTalkModalNickname />
-        <JoinTalkModalTeam leftTeam={leftTeam} rightTeam={rightTeam} />
+    <FormProvider {...form}>
+      <View style={styles.container}>
+        <JoinTalkModalHeader />
+        <View style={styles.bodyContainer}>
+          <JoinTalkModalProfileImage />
+          <JoinTalkModalNickname />
+          <JoinTalkModalTeam leftTeam={leftTeam} rightTeam={rightTeam} />
+        </View>
+        <View style={styles.footerContainer}>
+          <JoinTalkModalButton handleSubmit={() => {}} />
+        </View>
       </View>
-      <View style={styles.footerContainer}>
-        <JoinTalkModalButton handleSubmit={() => {}} />
-      </View>
-    </View>
+    </FormProvider>
   );
 };
 
---
+++ pages/talk-list/constants/joinTalkForm.ts
@@ -0,0 +1,11 @@
+export const JOIN_TALK_FORM_DEFAULT_VALUES = {
+  profileImageUrl: "",
+  nickname: "",
+  team: "",
+};
+
+export const JOIN_TALK_FORM_PATH = {
+  PROFILE_IMAGE_URL: "profileImageUrl",
+  NICKNAME: "nickname",
+  TEAM: "team",
+};
