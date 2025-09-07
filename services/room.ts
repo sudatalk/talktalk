e89b482e193cac00328d1c0cae87a36a8489ed87
joy.hwan <joy.hwan@kakaocorp.com>
2025-09-07 23:44:00 +0900

    feat : 토론방 조회 API 제작

--- services/room.ts
+++ services/room.ts
@@ -1,6 +1,10 @@
-import { PostRoomParams, RoomResponse } from "@/types/room";
+import { GetRoomDetailParams, PostRoomParams, RoomResponse } from "@/types/room";
 import axios, { AxiosResponse } from "axios";
 
 export function postRoom(params: PostRoomParams): Promise<AxiosResponse<RoomResponse>> {
   return axios.post<RoomResponse>(`http://localhost:8000/room`, params);
+}
+
+export function getRoomDetail(params: GetRoomDetailParams): Promise<AxiosResponse<RoomResponse>> {
+  return axios.get<RoomResponse>(`http://localhost:8000/room/${params.id}`);
 }
