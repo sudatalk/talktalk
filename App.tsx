fe1be7447f588fe52d8863dd7545515fe3434bce
joy.hwan <joy.hwan@kakaocorp.com>
2025-09-07 10:38:11 +0900

    feat : 앱 컴포넌트 버튼 모달 부착

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import CreateTalkModal from "./pages/talk-list/components/CreateTalkModal";
+import useDisclosure from "./hooks/useDisclosure";
 
 export default function App() {
+  const { isOpen, handleOpen, handleClose } = useDisclosure();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Button title="채팅방 만들기 오픈" onPress={handleOpen} />
+        <Button title="채팅방 참여 오픈" />
+        <CreateTalkModal isOpen={isOpen} handleClose={handleClose} />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
