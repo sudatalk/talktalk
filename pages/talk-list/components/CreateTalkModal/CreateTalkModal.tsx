6d6058b154b117793e7f447a4e2caa31bb969b75
joy.hwan <joy.hwan@kakaocorp.com>
2025-09-07 14:10:11 +0900

    feat : RHF 부착

--- pages/talk-list/components/CreateTalkModal/CreateTalkModal.tsx
+++ pages/talk-list/components/CreateTalkModal/CreateTalkModal.tsx
@@ -5,6 +5,7 @@ import Text from "@/components/Text";
 import CreateTalkModalTitle from "./CreateTalkModalTitle";
 import CreateTalkModalTeam from "../CreateTalkModalTeam";
 import CreateTalkModalDuration from "./CreateTalkModalDuration";
+import CreateTalkModalContent from "./CreateTalkModalContent";
 
 type Props = {
   isOpen: boolean;
@@ -15,47 +16,10 @@ const CreateTalkModal = (props: Props) => {
   const { isOpen, handleClose } = props;
 
   return (
-    <Modal isOpen={true} handleClose={handleClose}>
-      <View style={styles.container}>
-        <CreateTalkModalHeader />
-        <View style={styles.bodyContainer}>
-          <CreateTalkModalTitle />
-          <CreateTalkModalTeam />
-          <CreateTalkModalDuration />
-        </View>
-        <View style={styles.footerContainer}>
-          <Text white h3>
-            생성
-          </Text>
-        </View>
-      </View>
+    <Modal isOpen={isOpen} handleClose={handleClose}>
+      {isOpen && <CreateTalkModalContent />}
     </Modal>
   );
 };
 
 export default CreateTalkModal;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-
-    padding: 10,
-    paddingTop: 0,
-  },
-
-  headerContainer: {
-    flex: 0.15,
-
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  bodyContainer: {
-    flex: 0.7,
-  },
-  footerContainer: {
-    flex: 0.15,
-
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ pages/talk-list/components/CreateTalkModal/CreateTalkModalContent.tsx
@@ -0,0 +1,67 @@
+import { Pressable, StyleSheet, View } from "react-native";
+import CreateTalkModalHeader from "./CreateTalkModalHeader";
+import CreateTalkModalTitle from "./CreateTalkModalTitle";
+import CreateTalkModalTeam from "../CreateTalkModalTeam";
+import CreateTalkModalDuration from "./CreateTalkModalDuration";
+import Text from "@/components/Text";
+import { FormProvider, useForm } from "react-hook-form";
+import { CREATE_TALK_FORM_DEFAULT_VALUES } from "../../constants/createTalkForm";
+import { ON_SUBMIT } from "@/constants/form";
+
+const CreateTalkModalContent = () => {
+  const form = useForm({
+    defaultValues: CREATE_TALK_FORM_DEFAULT_VALUES,
+    mode: ON_SUBMIT,
+    reValidateMode: ON_SUBMIT,
+  });
+
+  const handleSubmit = form.handleSubmit(
+    (value) => {
+      console.log("value : ", value);
+    },
+    (error) => {
+      console.log("error : ", error);
+    }
+  );
+
+  return (
+    <FormProvider {...form}>
+      <View style={styles.container}>
+        <CreateTalkModalHeader />
+        <View style={styles.bodyContainer}>
+          <CreateTalkModalTitle />
+          <CreateTalkModalTeam />
+          <CreateTalkModalDuration />
+        </View>
+        <View style={styles.footerContainer}>
+          <Pressable onPress={handleSubmit} style={{ padding: 10, paddingLeft: 20, paddingRight: 20 }}>
+            <Text white h3>
+              생성
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </FormProvider>
+  );
+};
+
+export default CreateTalkModalContent;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    padding: 10,
+    paddingTop: 0,
+  },
+
+  bodyContainer: {
+    flex: 0.7,
+  },
+  footerContainer: {
+    flex: 0.15,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
