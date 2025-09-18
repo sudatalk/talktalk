bdb0e9163928f745f840a5cf748c6f8fc2bd5b97
joy.hwan <joy.hwan@kakaocorp.com>
2025-09-19 00:45:11 +0900

    feat : 방 참여 및 방 생성 API 수정 + 리패칭 로직 추가

--- pages/talk-list/components/CreateTalkModal/CreateTalkModal.tsx
+++ pages/talk-list/components/CreateTalkModal/CreateTalkModal.tsx
@@ -4,14 +4,15 @@ import CreateTalkModalContent from "./CreateTalkModalContent";
 type Props = {
   isOpen: boolean;
   handleClose: () => void;
+  handleRefetchRoomList: () => Promise<void>;
 };
 
 const CreateTalkModal = (props: Props) => {
-  const { isOpen, handleClose } = props;
+  const { isOpen, handleClose, handleRefetchRoomList } = props;
 
   return (
     <Modal isOpen={isOpen} handleClose={handleClose}>
-      {isOpen && <CreateTalkModalContent handleClose={handleClose} />}
+      {isOpen && <CreateTalkModalContent handleClose={handleClose} handleRefetchRoomList={handleRefetchRoomList} />}
     </Modal>
   );
 };
--- pages/talk-list/components/JoinTalkModal/JoinTalkModal.tsx
+++ pages/talk-list/components/JoinTalkModal/JoinTalkModal.tsx
@@ -2,16 +2,18 @@ import Modal from "@/components/Modal";
 import JoinTalkModalContent from "./JoinTalkModalContent";
 
 type Props = {
+  roomId?: number;
   isOpen: boolean;
   handleClose: () => void;
+  handleRefetchRoomList: () => Promise<void>;
 };
 
 const JoinTalkModal = (props: Props) => {
-  const { isOpen, handleClose } = props;
+  const { isOpen, roomId, handleClose, handleRefetchRoomList } = props;
 
   return (
     <Modal isOpen={isOpen} handleClose={handleClose}>
-      {isOpen && <JoinTalkModalContent handleClose={handleClose} />}
+      {isOpen && roomId && <JoinTalkModalContent roomId={roomId} handleClose={handleClose} handleRefetchRoomList={handleRefetchRoomList} />}
     </Modal>
   );
 };
