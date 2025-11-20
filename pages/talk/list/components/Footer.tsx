53ab23d40fe0f2464b093282681d3b8349f5f156
joy.hwan <joy.hwan@kakaocorp.com>
2025-11-20 17:40:26 +0900

    feat : 링크 수정

--- pages/talk/list/components/Footer.tsx
+++ pages/talk/list/components/Footer.tsx
@@ -7,38 +7,38 @@ import { View, Text, StyleSheet } from "react-native";
 export default function Footer() {
   return (
     <View style={styles.footer}>
-      <Pressable onPress={() => Linking.openURL("https://www.naver.com")}>
+      <Pressable onPress={() => Linking.openURL("https://forms.gle/mEAhmTD3E1VJyCHE8")}>
         <Text style={styles.footerText}>문의하기</Text>
       </Pressable>
       <View style={styles.footerDivider} />
-      <Pressable onPress={() => Linking.openURL("https://www.naver.com")}>
+      <Pressable onPress={() => Linking.openURL("https://docs.google.com/document/d/1OHw4yoKmL1qLxwM4_PKNOTxR8Q-6o2FQUz5hPNF-qXM/edit?usp=sharing")}>
         <Text style={styles.footerText}>개인정보처리방침</Text>
       </Pressable>
       <View style={styles.footerDivider} />
-      <Pressable onPress={() => Linking.openURL("https://www.naver.com")}>
-        <Text style={styles.footerText}>이용약관</Text>
+      <Pressable onPress={() => Linking.openURL("https://docs.google.com/document/d/1lUKgYrLwyP7314hDxoiVQhhZjXOA5Mac3A5WZUuPNIM/edit?usp=sharing")}>
+        <Text style={styles.footerText}>공지사항</Text>
       </Pressable>
     </View>
   );
 }
 const styles = StyleSheet.create({
-    footer: {
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      paddingVertical: 16,
-      borderTopWidth: 0.5,
-      borderTopColor: "rgba(255,255,255,0.2)",
-      backgroundColor: "#000",
-    },
-    footerText: {
-      color: "rgba(255,255,255,0.6)",
-      fontSize: 12,
-    },
-    footerDivider: {
-      width: 1,
-      height: 10,
-      backgroundColor: "rgba(255,255,255,0.2)",
-      marginHorizontal: 10,
-    },
-  });
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 16,
+    borderTopWidth: 0.5,
+    borderTopColor: "rgba(255,255,255,0.2)",
+    backgroundColor: "#000",
+  },
+  footerText: {
+    color: "rgba(255,255,255,0.6)",
+    fontSize: 12,
+  },
+  footerDivider: {
+    width: 1,
+    height: 10,
+    backgroundColor: "rgba(255,255,255,0.2)",
+    marginHorizontal: 10,
+  },
+});
